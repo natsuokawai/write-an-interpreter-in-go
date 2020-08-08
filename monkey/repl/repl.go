8cce808b7a3861886c3a0fcf9133b9c48243f7b7
natsuokawai <kawai.natsuo@gmail.com>
2020-08-08 16:33:46 +0900

    evaluate integer literal

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -7,6 +7,7 @@ import (
 	"monkey/lexer"
 	// "monkey/token"
 	"monkey/parser"
+	"monkey/evaluator"
 )
 
 const PROMPT = ">> "
@@ -35,8 +36,14 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		// io.WriteString(out, program.String())
+		// io.WriteString(out, "\n")
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
