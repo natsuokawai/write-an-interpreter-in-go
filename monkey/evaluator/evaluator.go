350167002f266f44c1cf34046e97a0312d3e5a7a
natsuokawai <kawai.natsuo@gmail.com>
2020-08-10 10:43:43 +0900

    eval if expression

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -15,6 +15,8 @@ func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
 		return evalStatemetns(node.Statements)
+	case *ast.BlockStatement:
+		return evalStatemetns(node.Statements)	
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
@@ -28,6 +30,8 @@ func Eval(node ast.Node) object.Object {
 		left  := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
@@ -124,5 +128,30 @@ func evalIntegerInfixExpression(
 		return nativeBoolToBooleanObject(left_val != right_val)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
